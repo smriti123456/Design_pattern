cf3785f2dde33fc77aea2ca4121a6dd2ec9bcfd6
smriti123456 <46773546+smriti123456@users.noreply.github.com>
2019-01-30 18:35:47 +0530

    Update Eager.java

--- Eager.java
+++ Eager.java
@@ -16,7 +16,7 @@ class temp {
 	Eager a = Eager.getInstance();
 	Eager b = Eager.getInstance();
 	System.out.println("Object1: " + a.str);
-	b.str = "Shivam Agarwal";
+	b.str = "Smriti Rastogi";
 	System.out.println("After Changes:");
 	System.out.println("Object1: " + a.str);
 	System.out.println("Object2: " + b.str);
